2094ccf0677d620cf607291baff0f67499c2c418
lelloman <cerasuolodomenico@gmail.com>
2017-09-26 07:49:23 +0200

    added many more languages

--- make_wikidataset.py
+++ make_wikidataset.py
@@ -5,6 +5,7 @@ import urllib2
 import json
 from os.path import join as path
 from utils import sanitize_text
+from datetime import timedelta
 
 WIKIDATASET_DIR = "wikidataset"
 LANGUAGES = [
@@ -14,7 +15,15 @@ LANGUAGES = [
     "fr",
     "it",
     "pt",
-    "nl"
+    "nl",
+
+    "sv",
+    "pl",
+    "vi",
+    "ru",
+    "uk",
+    "fi",
+    "hu"
 ]
 
 PAGES_TITLES = [
@@ -44,6 +53,7 @@ def setup_folder():
 
 
 def get_titles_from_english(english_title):
+    print "get langlinks for title <{}>".format(english_title)
     query = urllib2.urlopen(
         "https://en.wikipedia.org/w/api.php?action=query&titles={}&prop=langlinks&lllimit=500&format=json".format(
             english_title)).read()
@@ -71,25 +81,27 @@ def sanitize_file(filename):
 
 
 if __name__ == "__main__":
-    # setup_folder()
-    #
-    # print "fetching titles in all languages..."
-    # titles = [item for title in PAGES_TITLES for item in get_titles_from_english(title)]
-    # lang_sets = [
-    #     {"lang": lang, "titles": filter(lambda x: x['lang'] == lang, titles)} for lang in LANGUAGES
-    # ]
-    #
-    # lang_sets = {lang['lang']: [x['*'] for x in lang['titles']] for lang in lang_sets}
-    #
-    # print lang_sets
-    #
-    # for lang in LANGUAGES:
-    #     wiki.set_lang(lang)
-    #     print lang
-    #     with open(path(WIKIDATASET_DIR, lang), "wb") as f:
-    #         for title in lang_sets[lang]:
-    #             print "\twriting '{}'...".format(title.encode("UTF-8"))
-    #             f.write(wiki.page(title).content.encode("UTF-8") + "\n")
+    setup_folder()
+
+    print "fetching titles in all languages..."
+    titles = [item for title in PAGES_TITLES for item in get_titles_from_english(title)]
+    lang_sets = [
+        {"lang": lang, "titles": filter(lambda x: x['lang'] == lang, titles)} for lang in LANGUAGES
+    ]
+
+    lang_sets = {lang['lang']: [x['*'] for x in lang['titles']] for lang in lang_sets}
+
+    print lang_sets
+
+    wiki.set_rate_limiting(True, timedelta(seconds=1))
+
+    for lang in LANGUAGES:
+        wiki.set_lang(lang)
+        print lang
+        with open(path(WIKIDATASET_DIR, lang), "wb") as f:
+            for title in lang_sets[lang]:
+                print "\twriting '{}'...".format(title.encode("UTF-8"))
+                f.write(wiki.page(title).content.encode("UTF-8") + "\n")
 
     for lang in LANGUAGES:
         sanitize_file(path(WIKIDATASET_DIR, lang))
